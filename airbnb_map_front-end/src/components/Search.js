e3e691740f9b9f1479e70b433a284944f5a5bbbd
GoTo15 <github email address>
2020-04-16 09:40:35 -0500

    Add front end functionality

--- airbnb_map_front-end/src/components/Search.js
+++ airbnb_map_front-end/src/components/Search.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import CloseButton from './CloseButton';
-import mag from '../assets/mag_glass.svg';
+// import mag from '../assets/mag_glass.svg';
 import '../stylesheets/Search.css';
 
 class Search extends React.Component {
+  state = {
+    categories: ['lawn service', 'dog walking', 'toilet paper'],
+  };
+
+  fetchOptions = () => {};
+
+  createDropDown = (options) => {
+    return options.map((c, index) => {
+      return <option key={index}>{c}</option>;
+    });
+  };
+
   render() {
     return (
       <div id='search'>
-        <CloseButton className="exit-button" closeButton={this.props.closeButton} />
-        <div className="search-bar">
-          <img src={mag} alt='search' />
-          <input type='search' name='search' placeHolder="Search" />
+        <CloseButton className='exit-button' closeButton={this.props.closeButton} />
+        <div className='search-bar'>
+          Find By Category
+          <br/>
+          <br/>
+          <fieldset>
+            <select id='category-dd'>
+              {this.createDropDown(this.state.categories)}
+            </select>
+          </fieldset>
         </div>
       </div>
     );
--- airbnb_map_front-end/src/components/Services.js
+++ airbnb_map_front-end/src/components/Services.js
@@ -3,54 +3,108 @@ import '../stylesheets/Services.css';
 import CloseButton from './CloseButton';
 
 class History extends React.Component {
-  render() {
-    let user = {
-      name: 'Lawn Service',
-      phone: '(555)555-5555',
-      email: 'ben@ben.com',
-      hours: {
-        monday: '8:00am-8:00pm',
-        tuesday: '8:00am-8:00pm',
-        wednesday: '8:00am-8:00pm',
-        thursday: '8:00am-8:00pm',
-        friday: '8:00am-8:00pm',
-        saturday: '8:00am-8:00pm',
-        sunday: '8:00am-8:00pm',
-      },
-    };
+  state = {
+    services: [],
+    newService: false,
+  };
 
+  componentDidMount = () => {
+    this.fetchServices(this.props.user.id);
+  };
+
+  fetchServices = (userId) => {
+    fetch(`http://localhost:4000/api/users/${userId}/history`)
+      .then((resp) => resp.json())
+      .then((services) => this.setState({ services }));
+  };
+
+  editService = (service) => {
+    service.price = 666;
+    fetch(`http://localhost:4000/api/services/${service.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        price: service.price,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => {
+        console.log(resp);
+      });
+  };
+
+  deleteService = (serviceId) => {
+    fetch(`http://localhost:4000/api/services/${serviceId}`, {
+      method: 'DELETE',
+    })
+      .then((resp) => resp.json())
+      .then((resp) => console.log(resp));
+  };
+
+  closeNewService = () => {
+    console.log('CLOSE NEW SERVICE');
+    this.setState({ newService: false });
+  };
+
+  newService = () => {
+    console.log('NEW SERVICE');
+    this.setState({ newService: true });
+  };
+
+  displayServices = (services) => {
+    return services.map((s) => {
+      return (
+        <div className='content' key={s.id}>
+          <h2>{s.name}</h2>
+          <br />
+          <h3>{this.props.user.name}</h3>
+          <br />
+          <p>
+            phone: {s.phone}
+            <br />
+            email: {s.email}
+          </p>
+          <br />
+          <button
+            onClick={() => {
+              this.editService(s);
+            }}
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => {
+              this.deleteService(s.id);
+            }}
+          >
+            Delete
+          </button>
+          <hr />
+        </div>
+      );
+    });
+  };
+
+  render() {
+    let buttonStyles = {
+      paddingTop: '50px',
+    };
     return (
       <div id='services'>
         <CloseButton closeButton={this.props.closeButton} />
-        <div className="content">
-          <h2>{user.name}</h2>
-          <br />
-          <h3>Contact</h3>
-          <br />
-          <p>
-            phone: {user.phone}
-            <br />
-            email: {user.email}
-          </p>
-          <br />
-          <h3>Hours</h3>
-          <br />
-          <p>
-            monday: {user.hours.monday}
-            <br />
-            tuesday: {user.hours.monday}
-            <br />
-            wednesday: {user.hours.monday}
-            <br />
-            thursday: {user.hours.monday}
-            <br />
-            friday: {user.hours.monday}
-            <br />
-            saturday: {user.hours.monday}
-            <br />
-            sunday: {user.hours.monday}
-          </p>
+        {this.displayServices(this.state.services)}
+        <br />
+        <div style={buttonStyles}>
+          <button onClick={this.newService}>New Service</button>
+        </div>
+        {this.state.newService ? (
+          <div className='blade'>
+            <button onClick={() => this.closeNewService()}>Help</button>
           </div>
+        ) : null}
       </div>
     );
   }
